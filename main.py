2dffc3f6d18dbce48a2322ab4297c044953e6090
colyk <colykxer@gmail.com>
2019-12-24 15:49:48 +0100

    WIP

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 
 import logging
+import os
 
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
@@ -7,46 +8,45 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 logger = logging.getLogger(__name__)
 
 
-def start(update, context):
-    update.message.reply_text('on /start')
-
-
-def help(update, context):
-    update.message.reply_text('on /help')
-
-
-def echo(update, context):
-    update.message.reply_text(f'on unknown command {update.message.text}')
-
-
-def error(update, context):
-    logger.warning('Update "%s" caused error "%s"', update, context.error)
-
-
-def text_to_channel(updater, text, chat_id='@colykxer'):
-    updater.bot.sendMessage(chat_id=chat_id, text='Some content')
-
-
-def main():
-    updater = Updater("", use_context=True)
-    text_to_channel(updater, 'text')
-
-    dp = updater.dispatcher
-    dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler("help", help))
-
-    dp.add_handler(MessageHandler(Filters.text, echo))
-
-    dp.add_error_handler(error)
-
-    updater.start_polling()
-    updater.idle()
-
 class Bot:
     def __init__(self):
-        pass
+        token = os.getenv('telegram_token')
+        if token is None:
+            logger.error('Set "telegram_token" environment variable.')
+            logger.error('export telegram_token=example_token')
+            exit(os.EX_CONFIG)
+        self.updater = Updater(token, use_context=True)
+        self.set_commands_handlers()
+    
+    def run(self):
+        self.updater.start_polling()
+        self.updater.idle()
 
-        
+    def text_to_channel(self, chat_id, text):
+        self.updater.bot.sendMessage(chat_id=chat_id, text=text)
+    
+    def set_commands_handlers(self):
+        dp = self.updater.dispatcher
+
+        dp.add_handler(CommandHandler("start", self.on_start))
+        dp.add_handler(CommandHandler("help", self.on_help))
+
+        dp.add_handler(MessageHandler(Filters.text, self.on_unknown))
+        dp.add_error_handler(self.on_error)
+    
+    def on_start(self, update, context):
+        update.message.reply_text('on /start')
+
+    def on_help(self, update, context):
+        update.message.reply_text('on /help')
+
+    def on_unknown(self, update, context):
+        update.message.reply_text(f'on unknown command "{update.message.text}"')
+
+    def on_error(self, update, context):
+        logger.warning('Update "%s" caused error "%s"', update, context.error)
+
 
 if __name__ == '__main__':
-    main()
+    bot = Bot()
+    bot.run()
